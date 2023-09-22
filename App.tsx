d3f7e138c45a1a01f4f5c4df32de94d125124374
gorkavillara <gorkavillara@gmail.com>
2023-09-22 09:50:37 +0200

    Tema 05 - Iniciando con bgImage

--- App.tsx
+++ App.tsx
@@ -1,64 +1,11 @@
 import { StatusBar } from "expo-status-bar"
-import { useState } from "react"
-import {
-    StyleSheet,
-    Text,
-    View,
-    Button,
-    Alert,
-    TouchableHighlight
-} from "react-native"
+import { StyleSheet, View, ImageBackground } from "react-native"
+import bgImage from "./assets/bgImage.png"
 
 export default function App() {
-    const [contador, setContador] = useState(10)
-    // const contador = estado[0]
-    // const setContador = estado[1]
-    // let contador = 1
-
-    const sumaContador = () => {
-        console.log("Has presionado el botón")
-        // setContador(contador + 1) // setContador(41)
-        // setContador(contador + 1) // setContador(41)
-        // setContador(contador + 1) // setContador(41)
-        setContador((valorAnterior) => {
-            console.log("Sumando 1")
-            return valorAnterior + 1
-        })
-        // setContador(valorAnterior => {
-        //     console.log("Sumando 2")
-        //     return valorAnterior + 2
-        // })
-        // setContador(valorAnterior => {
-        //     console.log("Sumando 3")
-        //     return valorAnterior + 3
-        // })
-        // contador++
-        // console.log(contador)
-    }
-    const restaContador = () => setContador((prev) => prev - 1)
-
     return (
         <View style={styles.container}>
-            <Text style={styles.titulo}>Hola mundo</Text>
-            <Text style={{ fontSize: 24 }}>Contador: {contador}</Text>
-            <View style={{ flexDirection: "row", gap: 8 }}>
-                <Button title="menos" onPress={restaContador} />
-                <Button title="más" onPress={sumaContador} />
-            </View>
-            <TouchableHighlight
-                style={{
-                    paddingVertical: 8,
-                    paddingHorizontal: 16,
-                    backgroundColor: "#ee0099",
-                    borderRadius: 8
-                }}
-                onPress={() => Alert.alert("Has hecho click")}
-                disabled={false}
-                activeOpacity={0.5}
-                underlayColor="#ee00dd"
-            >
-                <Text>Botón personalizado</Text>
-            </TouchableHighlight>
+            <ImageBackground source={bgImage} style={styles.bg} />
             <StatusBar style="auto" />
         </View>
     )
@@ -66,11 +13,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: 16
+        flex: 1
+    },
+    bg: {
+        flex: 1
     },
     titulo: { fontSize: 32, color: "blue" }
 })
