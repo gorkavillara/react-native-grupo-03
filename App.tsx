858d5cba6aea159665245638f3e0f8c8d03660b2
gorkavillara <gorkavillara@gmail.com>
2023-09-29 11:56:10 +0200

    Tema 07 - Iniciando con estilos

--- App.tsx
+++ App.tsx
@@ -1,17 +1,10 @@
-import { View } from "react-native"
 import AppContextProvider from "./contexts/AppContextProvider"
-import SwitchDarkMode from "./components/common/SwitchDarkMode"
 import AppLayout from "./layouts/AppLayout"
-import ContadorConContexto from "./screens/ContadorConContexto"
-import ContadorIndividual from "./screens/ContadorIndividual"
-import ContadorConHook from "./screens/ContadorConHook"
-import RickMorty from "./screens/RickMorty/RickMortyMejorado"
 
 export default function App() {
     return (
         <AppContextProvider>
             <AppLayout>
-                <RickMorty />
             </AppLayout>
         </AppContextProvider>
     )
--- screens/index.ts
+++ screens/index.ts
@@ -1,6 +1,1 @@
-import ComponentsTest from "./ComponentsTest/ComponentsTest";
-import Contador from "./Contador/Contador";
-import ContadorSignal from "./ContadorSignal/ContadorSignal";
-import RickMorty from "./RickMorty/RickMorty";
-
-export { ComponentsTest, Contador, ContadorSignal, RickMorty }
+export {}
